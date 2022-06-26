aa4a1c55d3a46bf03d471ce343691a811101b1ce
Jaewon <woin2ee@gmail.com>
2022-06-26 22:42:16 +0900

    present Alert, ActionSheet

--- AlertExample/ViewController.swift
+++ AlertExample/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showAlertButtonDidTap(_ sender: Any) {
+        let alert = UIAlertController(title: "Title", message: "message", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "ok", style: .default) { _ in
+            print("ok")
+        }
+        alert.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "cancel", style: .cancel) { _ in
+            print("cancel")
+        }
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: false)
+    }
+    
+    @IBAction func showActionSheetButtonDidTap(_ sender: Any) {
+        let alert = UIAlertController(title: "Title", message: "message", preferredStyle: .actionSheet)
+        
+        let defaultAction = UIAlertAction(title: "default", style: .default) { _ in
+            print("default")
+        }
+        alert.addAction(defaultAction)
+        
+        let desctructiveAction = UIAlertAction(title: "destructive", style: .destructive) { _ in
+            print("destructive")
+        }
+        alert.addAction(desctructiveAction)
+        
+        let cancelAction = UIAlertAction(title: "cancel", style: .cancel) { _ in
+            print("cancel")
+        }
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: false)
+    }
 }
-
